d281a428a02ebef73e9b5de35a73c49d3570de48
Pádua Alves <paduaalves03@gmail.com>
2021-11-27 16:44:13 -0300

    Adicionando equals and hash code

--- src/br/com/alura/Aula.java
+++ src/br/com/alura/Aula.java
@@ -1,12 +1,14 @@
 package br.com.alura;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.ToString;
 
 @Getter
 @AllArgsConstructor
 @ToString
+@EqualsAndHashCode
 public class Aula implements Comparable<Aula> {
 
 	private String titulo;
